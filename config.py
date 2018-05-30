2639d336f3dd6f8cfff5cfc08036dc00c4fdfe25
Dragos Gegea <dragos_gegea@yahoo.com>
2018-05-30 18:23:41 +0300

    Upload

---
+++ config.py
@@ -0,0 +1,19 @@
+"""
+Configuration for the generate module
+"""
+
+FLAG_CPU_MODE = False
+
+paths = dict()
+
+# Encoder
+paths['vectormodels'] = '/media/dragos/ESD-USB/models/'
+paths['vectortables'] = '/media/dragos/ESD-USB/models/'
+
+# Decoder
+paths['decmodel'] = '/home/dragos/Final Year Project/helper/romance.npz'
+paths['dictionary'] = '/home/dragos/Final Year Project/helper/romance_dictionary.pkl'
+
+# Sentences
+paths['sentences'] = '/home/dragos/Sentences.txt'
+
